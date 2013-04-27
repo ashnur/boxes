858b8ac1446278433220aabf8bb28afefba0ed96
Ashnur <ashnur@gmail.com>
2013-04-27 13:33:48 +0300

    added redraw

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 void function(root){
+
+    function toString(box, locals){
+        return haml.execute(box.tpl, box, locals)
+    }
+
+    function element(box, force){
+        if ( force || !box.self ){
+            box.self = bonzo.create(toString(box))
+        }
+        return box.self
+    }
+
+    function draw(box){
+        var elem = element(box)
+        bonzo(elem).replaceWith(element(box, true))
+    }
+
+    function set(box, name, value){
+        box[name] = value
+        bean.fire(boxes, 'draw', [box])
+    }
+
+    function appendTo(box, elem){ bonzo(elem).append(element(box)) }
+    function prependTo(box, elem){ bonzo(elem).prepend(element(box)) }
+
     var viral = require('viral')
         , haml= require('haml')
+        , u = require('totemizer')
         , boxes = viral.extend({
-            init: function(tpl){ this.tpl = haml.optimize(haml.compile(tpl))}
-            , toString: function(){ return haml.execute(this.tpl, this) }
-
+            init: function(tpl){ this.tpl = haml.optimize(haml.compile(tpl)) }
+            , toString: u.enslave(toString)
+            , element: u.enslave(element)
+            , set: u.enslave(set)
+            , draw: u.enslave(draw)
+            , appendTo: u.enslave(appendTo)
         })
+        , bonzo = require('bonzo')
+        , bean = require('bean')
         ;
 
-    window.addEventListener('load', function(){
-
+    bean.on(boxes, 'draw', function(box){
+        draw(box)
     })
 
     if ( module !== undefined && module.exports ) {
--- test/test.js
+++ test/test.js
@@ -2,30 +2,39 @@ void function(root){
     var boxes = require('../')
         , expect = require('expect.js')
         , fs = require('fs')
-        , tpl1 = '#test1\n  %p= this.content'
-        , tpl2 = '#test2 != this.content'
-        , tpl3 = '#test3 != this.content'
+        , tpl1 = '#test1.box \n  %p= this.content'
+        , tpl2 = '#test2.box  != this.content'
+        , tpl3 = '#test3.box  != this.content'
         , box1 = boxes.make(tpl1)
         , box2 = boxes.make(tpl2)
         , box3 = boxes.make(tpl3)
+        , bonzo = require('bonzo')
         ;
 
 
     describe('boxes', function(){
         it('expects to compile the variable into the template', function(){
             box1.content = 'whatever'
-            expect(box1+'').to.be('<div id="test1"><p>whatever</p></div>')
+            expect(box1+'').to.be('<div class="box" id="test1"><p>whatever</p></div>')
         })
 
         it('expects boxes to be embeddadle ', function(){
             box2.content = box3
             box3.content = box1
             box1.content = 'dafuq'
-            expect(box2+'').to.be('<div id="test2"><div id="test3"><div id="test1"><p>dafuq</p></div></div></div>')
+            expect(box2+'').to.be('<div class="box" id="test2"><div class="box" id="test3"><div class="box" id="test1"><p>dafuq</p></div></div></div>')
         })
 
-        it('expects boxes to update the dom when content is changed', function(){
-
+        it('expects boxes to beadded to the dom', function(done){
+            box1.content = 'whatever'
+            box1.appendTo(document.body)
+            expect(document.getElementById('test1').outerHTML).to.be('<div class="box" id="test1"><p>whatever</p></div>')
+            done()
+        })
+        it('expects boxes to update the dom when content is changed', function(done){
+            box1.set('content', 'dafuq')
+            expect(document.getElementById('test1').outerHTML).to.be('<div class="box" id="test1"><p>dafuq</p></div>')
+            done()
         })
     })
 
